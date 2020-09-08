4144924ebb934482b4f5ef6fc30cd80dbe815333
土本泰之 <tsuchy1625@gmail.com>
2020-09-08 16:38:41 +0900

    ignore

--- src/components/id/index.js
+++
@@ -1,7 +0,0 @@
-
-export const id= 'puxDwn2GbSemVg7J07iLCce8kuj1';
-export const key='01948a05fefdc190e5cf6abedc91790a15bfe15db2a2f5ae98dcd4f395abddcd';
-
-export const tamanoId= 'z9ZbzfRC7UdNzDw8h7qPuvWHdWh2';
-
-export const tamanoKey='d7764a23ab1740fd839e576fdb62b5634853db32d716ca543e76b3258540a2f9';
